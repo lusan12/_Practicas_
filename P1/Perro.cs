fbcc5202a661e119d90328a3b29d0cb178be9650
LuSan <30853030+lusan12@users.noreply.github.com>
2018-08-19 17:30:50 -0500

    Add files via upload

---
+++ P1/Perro.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mascotas
+{
+    class Perro
+    {
+
+        public string Tamaño;
+        public string Raza;
+        public string Sexo;
+        public int Peso;
+        public string Color;
+        public enum TIPOSDECOMIDA { CROQUETAS, BASURA, AGUA };
+
+        public Perro ()
+        {
+            Raza = "Pastor Aleman";
+            Peso |= 18;
+            Sexo = "Macho";
+            Tamaño = "Grande";
+        }
+        public Perro(string raza, int peso, string sexo, string tamaño)
+        {
+            Raza = "Corriente";
+            Peso |= 7;
+            Sexo = "Macho";
+            Tamaño = "Mediano";
+        }
+        public void Ladrar()
+        {
+
+        }
+
+        public void Correr()
+        {
+
+        }
+
+        public bool Comer(int alimento)
+        {
+
+
+            if (alimento == (int)TIPOSDECOMIDA.AGUA)
+            {
+                return true;
+            }
+            else if (alimento == (int)TIPOSDECOMIDA.BASURA)
+            {
+                return true;
+            }
+            else if (alimento == (int)TIPOSDECOMIDA.CROQUETAS)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public void Cagar()
+        {
+
+
+        }
+    }
+}
+
---
+++ P1/Program.cs
@@ -0,0 +1,42 @@
+﻿////////////////program.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mascotas
+{
+    class Program
+    {
+        enum TIPOSDECOMIDA { CROQUETAS, BASURA, AGUA }
+
+        static void Main(string[] args)
+
+
+        {
+            bool alimentado = false;
+
+            Perro Perro1 = new Perro();
+
+            Perro1.Raza = "Corriente";
+            Perro1.Peso |= 7;
+            Perro1.Sexo = "Macho";
+            Perro1.Tamaño = "Mediano";
+
+            alimentado = Perro1.Comer((int)TIPOSDECOMIDA.AGUA);
+
+          ///  if (alimentado)
+                //Console.WriteLine("El perro " + Perro1.Raza + "comio" + Convert.ToString(TIPOSDECOMIDA.AGUA));
+
+
+           // Console.WriteLine("EL perro no ha comido");
+           // Console.ReadKey();
+            Perro Perro2 = new Perro();
+            Console.WriteLine("El perro " + Perro2.Raza + "comio" + Convert.ToString(TIPOSDECOMIDA.AGUA));
+            Console.Read();
+
+        }
+
+    }
+}
